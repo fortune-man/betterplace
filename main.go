f0e4c0b6cfcd82a4f3eaf4a82a787330168e36da
fortune-man <wnguddl96@knou.ac.kr>
2024-06-17 18:29:19 +0900

    feat : info / projects / archive 클릭시 각 페이지로 이동

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+	// 정적 파일 서빙
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// INFO 페이지
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/info.html")
 	})
-	http.ListenAndServe(":8080", nil)
+
+	// PROJECTS 페이지
+	http.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/projects.html")
+	})
+
+	// ARCHIVE 페이지
+	http.HandleFunc("/archive", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/archive.html")
+	})
+
+	// 기본 페이지 설정 (index.html 이라면, / 로 접근시 index.html 로 이동)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/static/info.html", http.StatusMovedPermanently)
+	})
+
+	log.Println("서버가 포트 8080에서 실행 중입니다...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
